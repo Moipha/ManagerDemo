351168e1eee856b3e6f3e396fb3e2aa18883db66
Moipha <166232982+Moipha@users.noreply.github.com>
2024-09-12 17:42:00 +0800

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
-// export const serverIp = "zhixiao.site:5218"
-export const serverIp = "39.107.246.56:5218"
-export const protocol = "http"
+export const serverIp = "zhixiao.site:5218"
+// export const serverIp = "39.107.246.56:5218"
+export const protocol = "https"
 //TODO 此处在部署时需要修改
